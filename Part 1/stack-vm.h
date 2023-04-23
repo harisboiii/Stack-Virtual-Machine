61765e8c8c83f0f9292331a240e9ff5fa373a6fd
Muhammad Haris <125253097+harisboi@users.noreply.github.com>
2023-04-23 18:29:23 +0500

    Update stack-vm.h

--- Part 1/stack-vm.h
+++ Part 1/stack-vm.h
@@ -1,3 +1,5 @@
+// This header file defines a StackVM class that emulates a simple stack-based virtual machine
+
 #ifndef STACK_VM_H
 #define STACK_VM_H
 
@@ -7,27 +9,30 @@
 // type definitions
 typedef int32_t i32;
 
+// StackVM class declaration
 class StackVM {
-	i32 pc = 100; // program counter
-	i32 sp = 0; // stack pointer
-	std::vector<i32> memory;
-	i32 typ = 0;
-	i32 dat = 0;
-	i32 running = 1;
+// Private member variables
+i32 pc = 100; // program counter
+i32 sp = 0; // stack pointer
+std::vector<i32> memory; // memory array
+i32 typ = 0; // instruction type
+i32 dat = 0; // instruction data
+i32 running = 1; // flag to indicate whether the program is still running
 	
-	// private function
-	i32 getType(i32 instruction);
-	i32 getData(i32 instruction);
-	void fetch();
-	void decode();
-	void execute();
-	void doPrimitive();
-	
-	public:
-	// public functions
-	StackVM();
-	void run();
-	void loadProgram(std::vector<i32> prog);
+// Private member functions
+i32 getType(i32 instruction); // Extracts the instruction type from an instruction word
+i32 getData(i32 instruction); // Extracts the instruction data from an instruction word
+void fetch(); // Fetches the next instruction from memory
+void decode(); // Decodes the fetched instruction
+void execute(); // Executes the decoded instruction
+void doPrimitive(); // Executes a primitive instruction
+
+public:
+// Public member functions
+StackVM(); // Constructor function
+void run(); // Executes the loaded program
+void loadProgram(std::vector<i32> prog); // Loads a program into memory
+
 };
 
 #endif
