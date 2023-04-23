86cdea4791b7e09cd270e6da98a30f92b047be32
Muhammad Haris <125253097+harisboi@users.noreply.github.com>
2023-04-23 17:28:19 +0500

    Update main.cpp

--- Part 1/main.cpp
+++ Part 1/main.cpp
@@ -1,9 +1,14 @@
-#include "stack-vm.h"
+#include "stack-vm.h"  // Include the StackVM header file
 
 int main() {
-	StackVM vm;
+	StackVM vm;  // Create a StackVM instance called "vm"
+	
+	// Create a program represented as a vector of integers
+	// This program adds the values 3 and 4 and stores the result in memory location 0x40000000
 	std::vector<i32> prog{3, 4, 0x40000001, 0x40000000};
-	vm.loadProgram(prog);
-	vm.run();
-	return 0;
+	
+	vm.loadProgram(prog);  // Load the program into the virtual machine
+	vm.run();  // Run the virtual machine
+	
+	return 0;  // Return 0 to indicate successful program execution
 }
