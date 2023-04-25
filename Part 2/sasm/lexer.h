c0eb2ee171303ad0b4fe6547519fa747daf660ad
Muhammad Haris <125253097+harisboi@users.noreply.github.com>
2023-04-25 10:46:16 -0700

    Update lexer.h

--- Part 2/sasm/lexer.h
+++ Part 2/sasm/lexer.h
@@ -4,26 +4,31 @@
 #include <iostream>
 #include <vector>
 
+// Define a new data type "byte" as an alias for uint8_t and "strings" as an alias for std::vector<std::string>
 typedef uint8_t byte;
 typedef std::vector<std::string> strings;
 
+// Define an enumeration type "State" that represents the different states of the lexer
 enum State : byte {
-	START,
-	READCHAR,
-	READBLOCK,
-	SKIP,
-	DUMP,
-	COMMENT,
-	END
-}
+	START,      // Starting state
+	READCHAR,   // Reading a character state
+	READBLOCK,  // Reading a block of characters state
+	SKIP,       // Skipping spaces state
+	DUMP,       // Dumping characters state
+	COMMENT,    // Comment state
+	END         // End of input state
+};
 
+// The Lexer class
 class Lexer {
-	bool my_issspace(char c);
-	bool isspecial(char c);
-	bool isgroup(char c);
-	char end_char, beg_char;
+	// Private member functions
+	bool my_issspace(char c);   // Check if a character is a space character
+	bool isspecial(char c);     // Check if a character is a special character
+	bool isgroup(char c);       // Check if a character is a group character
+	char end_char, beg_char;    // Ending and beginning characters for a block of characters
 	public:
-	strings lex(std::string s);
+	// Public member functions
+	strings lex(std::string s); // Tokenize a given string and return the resulting tokens as a vector of strings
 };
 
 #endif
