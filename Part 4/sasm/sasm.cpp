36ed522c840abe79c94c293ce173feb351976300
Muhammad Haris <125253097+harisboi@users.noreply.github.com>
2023-05-01 16:53:31 +0500

    Update sasm.cpp

--- Part 4/sasm/sasm.cpp
+++ Part 4/sasm/sasm.cpp
@@ -2,61 +2,65 @@
 #include <fstream>
 #include "lexer.h"
 
-typdef uint32_t i32;
+typedef uint32_t i32; // Define i32 as an unsigned 32-bit integer
 
 using namespace std;
 
-// functions
+// Function prototypes
 vector<i32> compileToInstructions(strings s);
 bool isInteger(string s);
 bool isPrimimitive(string s);
-i32 mapToNumber(string s); 
+i32 mapToNumber(string s);
 
 int main(int argc, char *argv[]) {
-	// check for input errors
+	// Check if the program was run with exactly one argument
 	if (argc != 2) {
 		cout << "Usage: " << argv[0] << " <sasm-filename>" << endl;
-		exit(1);
+		exit(1); // Exit with an error code
 	}
 
-	// read input file
+	// Open the input file
 	ifstream infile;
 	infile.open(argv[1]);
 	if (!infile.is_open()) {
 		cout << "Error: could not open [" << argv[1] << "]" << endl;
-		exit(1);
+		exit(1); // Exit with an error code
 	}
+
+	// Read the entire file into a string
 	string line;
 	string contents;
 	while (getline(infile, line)) {
 		contents += line + "\n";
 	}
-	infile.close();
+	infile.close(); // Close the input file
 
-	// parse file
+	// Tokenize the input string using the Lexer class
 	Lexer lexer;
 	strings lexemes = lexer.lex(contents);
 
-	// compile to binary
+	// Convert the tokens to machine instructions
 	vector<i32> instructions = compileToInstructions(lexemes);
 
-	// write to binary file
+	// Write the machine instructions to a binary file
 	ofstream ofile;
 	ofile.open("out.bin", ios::binary);
 	for (i32 i = 0; i < instructions.size(); i++) {
 		ofile.write(reinterpret_cast<char *>(&instructions[i]), sizeof(i32));
 	}
-	ofile.close();
-	return 0;
+	ofile.close(); // Close the output file
+
+	return 0; // Exit with success
 }
 
+// Convert an array of tokens to an array of machine instructions
 vector<i32> compileToInstructions(strings s) {
 	vector<i32> instructions;
 	for (i32 i = 0; i < s.size(); i++) {
-		if (isInteger(s[i])) {
+		if (isInteger(s[i])) { // If the token is an integer, convert it to a machine word
 			instructions.push_back(stoi(s[i]));
 		} else {
-			i32 instruction = mapToNumber(s[i]);
+			i32 instruction = mapToNumber(s[i]); // Otherwise, map the token to a machine instruction
 			if (instruction != -1) {
 				instructions.push_back(instruction);
 			} else {
@@ -66,6 +70,8 @@ vector<i32> compileToInstructions(strings s) {
 	}
 	return instructions;
 }
+
+// Check if a string represents an integer
 bool isInteger(string s) {
 	for (i32 i = 0; i < s.length(); i++) {
 		if (!isdigit(s[i])) {
@@ -74,6 +80,13 @@ bool isInteger(string s) {
 	}
 	return true;
 }
+
+// Check if a string represents a primitive operation (+, -, *, /)
+bool isPrimimitive(string s) {
+	return (s == "+" || s == "-" || s == "*" || s == "/");
+}
+
+// Map a string to a machine instruction
 i32 mapToNumber(string s) {
 	if (s == "+") {
 		return 0x40000001;
@@ -84,5 +97,5 @@ i32 mapToNumber(string s) {
 	} else if (s == "/") {
 		return 0x40000004;
 	}
-	return -1; // invalid instruction
+	return -1; // Invalid instruction
 }
