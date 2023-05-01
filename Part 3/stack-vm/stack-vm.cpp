cd5952e399848bfa01b53b376e04cb52bf7fa2dd
Muhammad Haris <125253097+harisboi@users.noreply.github.com>
2023-05-01 16:30:29 +0500

    Update stack-vm.cpp

--- Part 3/stack-vm/stack-vm.cpp
+++ Part 3/stack-vm/stack-vm.cpp
@@ -1,86 +1,119 @@
+// Include the header file for the StackVM class
 #include "stack-vm.h"
 
 /*
- * Instruction format
- * header: 2 bits
- * data: 30 bits
- *
- * header format:
- * 0 => positive integer
- * 1 => primitive instruction
- * 2 => negative integer
- * 3 => undefined
- * */
 
-// functions
-StackVM::StackVM() {
-	memory.reserve(1000000);
+Instruction format
+header: 2 bits
+data: 30 bits
+header format:
+0 => positive integer
+1 => primitive instruction
+2 => negative integer
+3 => undefined
+*/
+// Constructor for StackVM class, reserve memory of size 1000000
+StackVM::StackVM()
+{
+    memory.reserve(1000000);
 }
-i32 StackVM::getType(i32 instruction) {
-	i32 type = 0xc0000000;
-	type = (type & instruction) >> 30;
-	return type;
-}
-i32 StackVM::getData(i32 instruction) {
-	i32 data = 0x3fffffff;
-	data = data & instruction;
-	return data;
-}
-void StackVM::fetch() {
-	pc++;
-}
-void StackVM::decode() {
-	typ = getType(memory[pc]);
-	dat = getData(memory[pc]);
-}
-void StackVM::execute() {
-	if (typ == 0 || typ == 2) {
-		sp++;
-		memory[sp] = dat;
-	} else {
-		doPrimitive();
-	}
-}
-void StackVM::doPrimitive() {
-	switch (dat) {
-		case 0: // halt
-			std::cout << "halt" << std::endl;
-			running = 0;
-			break;
-		case 1: // add
-			std::cout << "add " << memory[sp - 1] << " " << memory[sp] << std::endl;
-			memory[sp - 1] = memory[sp - 1] + memory[sp];
-			sp--;
-			break;
-		case 2: // sub
-			std::cout << "sub " << memory[sp - 1] << " " << memory[sp] << std::endl;
-			memory[sp - 1] = memory[sp - 1] - memory[sp];
-			sp--;
-			break;
-		case 3: // mul
-			std::cout << "mul " << memory[sp - 1] << " " << memory[sp] << std::endl;
-			memory[sp - 1] = memory[sp - 1] * memory[sp];
-			sp--;
 
-			break;
-		case 4: // div
-			std::cout << "div " << memory[sp - 1] << " " << memory[sp] << std::endl;
-			memory[sp - 1] = memory[sp - 1] / memory[sp];
-			sp--;
-			break;
-	}
+// Get the type of instruction using bit manipulation
+i32 StackVM::getType(i32 instruction)
+{
+    i32 type = 0xc0000000;
+    type = (type & instruction) >> 30;
+    return type;
 }
-void StackVM::run() {
-	pc -= 1;
-	while (running == 1) {
-		fetch();
-		decode();
-		execute();
-		std::cout << "tos: " << memory[sp] << std::endl;
-	}
+
+// Get the data of instruction using bit manipulation
+i32 StackVM::getData(i32 instruction)
+{
+    i32 data = 0x3fffffff;
+    data = data & instruction;
+    return data;
 }
-void StackVM::loadProgram(std::vector<i32> prog) {
-	for (i32 i = 0; i < prog.size(); i++) {
-		memory[pc + i] = prog[i];
-	}	
+
+// Increment the program counter
+void StackVM::fetch()
+{
+    pc++;
+}
+
+// Decode the instruction and get its type and data
+void StackVM::decode()
+{
+    typ = getType(memory[pc]);
+    dat = getData(memory[pc]);
+}
+
+// Execute the instruction
+void StackVM::execute()
+{
+    // If the type of instruction is 0 or 2, push the data onto the stack
+    if (typ == 0 || typ == 2)
+    {
+        sp++;
+        memory[sp] = dat;
+    }
+    // Otherwise, perform a primitive operation
+    else
+    {
+        doPrimitive();
+    }
+}
+
+// Perform a primitive operation based on the data of the instruction
+void StackVM::doPrimitive()
+{
+    switch (dat)
+    {
+    case 0: // halt
+        std::cout << "halt" << std::endl;
+        running = 0;
+        break;
+    case 1: // add
+        std::cout << "add " << memory[sp - 1] << " " << memory[sp] << std::endl;
+        memory[sp - 1] = memory[sp - 1] + memory[sp];
+        sp--;
+        break;
+    case 2: // sub
+        std::cout << "sub " << memory[sp - 1] << " " << memory[sp] << std::endl;
+        memory[sp - 1] = memory[sp - 1] - memory[sp];
+        sp--;
+        break;
+    case 3: // mul
+        std::cout << "mul " << memory[sp - 1] << " " << memory[sp] << std::endl;
+        memory[sp - 1] = memory[sp - 1] * memory[sp];
+        sp--;
+        break;
+    case 4: // div
+        std::cout << "div " << memory[sp - 1] << " " << memory[sp] << std::endl;
+        memory[sp - 1] = memory[sp - 1] / memory[sp];
+        sp--;
+        break;
+    }
+}
+
+// Run the program
+void StackVM::run()
+{
+    pc -= 1; // Set the program counter to -1
+    while (running == 1)
+    {
+        fetch();                                         // Increment the program counter
+        decode();                                        // Decode the instruction
+        execute();                                       // Execute the instruction
+        std::cout << "tos: " << memory[sp] << std::endl; // Print the top of stack
+    }
+}
+
+// Load a program into memory
+
+void StackVM::loadProgram(std::vector<i32> prog)
+{
+    for (i32 i = 0; i < prog.size(); i++)
+    {
+        memory[pc + i] = prog[i];
+    }
 }
