0ddf369e3701334629e6654fb1b30ca895534f01
Muhammad Haris <125253097+harisboi@users.noreply.github.com>
2023-05-02 02:03:03 +0500

    Update main.cpp

--- Part 5/stack-vm/main.cpp
+++ Part 5/stack-vm/main.cpp
@@ -1,9 +1,10 @@
 #include "stack-vm.h"
 
 int main() {
-	StackVM vm;
+	StackVM vm;  // create an instance of StackVM
+	// create a vector of instructions, in this case adding, subtracting, multiplying, and dividing integers
 	std::vector<i32> prog{3, 4, 0x40000001, 5, 0x40000002, 3, 0x40000003, 2, 0x40000004, 0x40000000};
-	vm.loadProgram(prog);
-	vm.run();
+	vm.loadProgram(prog);  // load the program into the StackVM instance
+	vm.run();  // execute the program in the StackVM instance
 	return 0;
 }
