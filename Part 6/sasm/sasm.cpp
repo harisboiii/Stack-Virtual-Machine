3b8728bbf9f86ab4114a26cf79f4c0502cdebf7f
Muhammad Haris <125253097+harisboi@users.noreply.github.com>
2023-05-03 22:45:20 +0500

    Update sasm.cpp

--- Part 6/sasm/sasm.cpp
+++ Part 6/sasm/sasm.cpp
@@ -6,7 +6,7 @@ typedef uint32_t i32;
 
 using namespace std;
 
-// functions
+// function declarations
 vector<i32> compileToInstructions(strings s);
 bool isInteger(string s);
 bool isPrimimitive(string s);
@@ -50,12 +50,17 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
+// function definitions
+
+// Compile the given sequence of lexemes into machine instructions
 vector<i32> compileToInstructions(strings s) {
 	vector<i32> instructions;
 	for (i32 i = 0; i < s.size(); i++) {
 		if (isInteger(s[i])) {
 			instructions.push_back(stoi(s[i]));
+			// add integer values as is
 		} else {
+			// map non-integer values to machine instructions
 			i32 instruction = mapToNumber(s[i]);
 			if (instruction != -1) {
 				instructions.push_back(instruction);
@@ -64,9 +69,12 @@ vector<i32> compileToInstructions(strings s) {
 			}
 		}
 	}
-	instructions.push_back(0x40000000); // always halt at the end
+	// halt instruction at the end
+	instructions.push_back(0x40000000);
 	return instructions;
 }
+
+// Check if the given string represents an integer
 bool isInteger(string s) {
 	for (i32 i = 0; i < s.length(); i++) {
 		if (!isdigit(s[i])) {
@@ -75,6 +83,8 @@ bool isInteger(string s) {
 	}
 	return true;
 }
+
+// Map given primitive operator to its corresponding machine instruction
 i32 mapToNumber(string s) {
 	if (s == "+") {
 		return 0x40000001;
