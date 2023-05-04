8f5274a524097618b0cfa77b2d39a5fd384f8747
Muhammad Haris <125253097+harisboi@users.noreply.github.com>
2023-05-05 01:42:57 +0500

    Update main.cpp

--- Part 6/stack-vm/main.cpp
+++ Part 6/stack-vm/main.cpp
@@ -1,25 +1,36 @@
-#include <iostream>
-#include <fstream>
-#include "stack-vm.h"
+// This program takes a filename as an argument and reads in binary instructions from the file to execute on a virtual machine.
+
+#include <iostream>  // Header file for input/output stream operations.
+#include <fstream>   // Header file for file input/output operations.
+#include "stack-vm.h"  // Header file for StackVM class.
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	if (argc < 2) {
-		cout << "Usage: " << argv[0] << " <filename>" << endl;
-		return 0;
-	}
+    // Check if a filename argument was provided.
+    if (argc < 2) {
+        cout << "Usage: " << argv[0] << " <filename>" << endl; // Display usage message.
+        return 0;  // Exit program.
+    }
 
-	ifstream r(argv[1], ios::binary);
-	i32 i;
-	vector<i32> prog;
+    // Open the binary file for reading.
+    ifstream r(argv[1], ios::binary);
 
-	while (r.read((char*)&i, sizeof(i))) {
-		prog.push_back(i);
-	}
+    // Declare variables.
+    i32 i;
+    vector<i32> prog;
 
-	StackVM vm;
-	vm.loadProgram(prog);
-	vm.run();
-	return 0;
+    // Read binary instructions from the file and store them in a vector.
+    while (r.read((char*)&i, sizeof(i))) {
+        prog.push_back(i);
+    }
+
+    // Create a new StackVM object and load the program into it.
+    StackVM vm;
+    vm.loadProgram(prog);
+
+    // Run the virtual machine.
+    vm.run();
+
+    return 0;  // Exit program.
 }
